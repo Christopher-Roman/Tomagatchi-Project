5de2560bed1f46e69e27141ce809877785f64145
Chris Roman <Christopherroman85@gmail.com>
2018-09-24 21:37:08 -0500

    psuedocode started

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// console.log('it is linked');
+
+// I am going to need to create a class for the tomagatchi
+	//the class will need to display the following:
+	//name
+	//age
+	//hunger 
+	//sleepiness
+	//boredom
+
+// The class will also need several methods
+	//A method that increases the age every few minutes
+	//sleepiness method that has the sleepiness increase every few minutes
+	//hunger method that has the hunger icnrease every few minutes
+	//boredom method that inceases the bordom every few minutes
+	//death method that kills the tomigatchi if any of the stats reach 10
+
+// It will also need a method that will morph it into something after it reaches a certain age
+	// what ability do I want it to have?
+
+// I will also need to build a method that allows it to have a baby once it reaches a certain age
+	// I can make 
+	// I can have it lay an egg and make the egg hatch after a certain amount of time
+	// I can add a method to the baby class that gives it a random ability once it hatches
+
+// I will need to create buttons that decrease the various stats based on what they are
+	// A button to feed the pet that will decrease hunger
+	// A button to play with the pet that will decrease boredom
+	// A button put the pet to bed that will decrease sleepiness
+
